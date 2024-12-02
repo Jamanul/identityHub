cd2f08dcf61f58e5a85601ae516d16263f22a43d
jamanul <jk.skb.25@gmail.com>
2024-12-02 16:11:50 +0600

    slider with swiper js added

--- js/main.js
+++ js/main.js
@@ -1,51 +1,52 @@
-let list =document.querySelector(".slider .list")
-//console.log(list)
-let items =document.querySelectorAll(".slider .list .item")
-//console.log(items)
-let dots =document.querySelectorAll(".slider .dots li")
-//console.log(dots)
-let prev =document.getElementById("prev")
-let next =document.getElementById("next")
-let active =0
-let  lengthItems = items.length -1
-next.onclick =function(){
-   if(active +1>lengthItems) {
-    active=0
-   }
-   else{
-    active =active+1
-   }
-     reloadSlider()
-}
-prev.onclick=function(){
-    if(active-1<0){
-        active=0
-    }
-    else{
-        active=active-1
-    }
-    reloadSlider()
-}
-let refreshslider =setInterval(()=>{next.click()},7000)
-const reloadSlider =()=>{
-    let checkleft = items[active].offsetLeft
-    list.style.left = -checkleft + 'px'
-    console.log(checkleft)
-    let lastActiveDot = document.querySelector(".slider .dots li.active-one")
-    lastActiveDot.classList.remove('active-one')
-    dots[active].classList.add('active-one')
 
-    clearInterval(refreshslider)
-    refreshslider =setInterval(()=>{next.click()},7000)
-}
+// let list =document.querySelector(".slider .list")
+// //console.log(list)
+// let items =document.querySelectorAll(".slider .list .item")
+// //console.log(items)
+// let dots =document.querySelectorAll(".slider .dots li")
+// //console.log(dots)
+// let prev =document.getElementById("prev")
+// let next =document.getElementById("next")
+// let active =0
+// let  lengthItems = items.length -1
+// next.onclick =function(){
+//    if(active +1>lengthItems) {
+//     active=0
+//    }
+//    else{
+//     active =active+1
+//    }
+//      reloadSlider()
+// }
+// prev.onclick=function(){
+//     if(active-1<0){
+//         active=0
+//     }
+//     else{
+//         active=active-1
+//     }
+//     reloadSlider()
+// }
+// let refreshslider =setInterval(()=>{next.click()},7000)
+// const reloadSlider =()=>{
+//     let checkleft = items[active].offsetLeft
+//     list.style.left = -checkleft + 'px'
+//     console.log(checkleft)
+//     let lastActiveDot = document.querySelector(".slider .dots li.active-one")
+//     lastActiveDot.classList.remove('active-one')
+//     dots[active].classList.add('active-one')
 
-dots.forEach((li,key)=>{
+//     clearInterval(refreshslider)
+//     refreshslider =setInterval(()=>{next.click()},7000)
+// }
 
-    li.addEventListener('click',function(){
-        active=key
-        reloadSlider()
-    })
-})
+// dots.forEach((li,key)=>{
+
+//     li.addEventListener('click',function(){
+//         active=key
+//         reloadSlider()
+//     })
+// })
 
 
 
@@ -53,6 +54,7 @@ dots.forEach((li,key)=>{
 
 // for responsive navbar
 const toggleButton = document.getElementById("toggleButton");
+console.log(toggleButton)
 const section = document.getElementById("responsive-navbar");
 const listItems = document.querySelectorAll(".responsive-navbar li");
 
